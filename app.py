764ebc4f78d8832c8777f6c5c0e4422f7128c394
mahmud aremu <aremumahmud2003@gmail.com>
2023-12-29 09:47:39 +0100

    added program files

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, request, jsonify
+from voice_identification import VoiceIdentification  # Assuming VoiceIdentification is implemented in voice_identification.py
+
+app = Flask(__name__)
+
+voice_id_system = VoiceIdentification()
+
+@app.route('/enroll', methods=['POST'])
+def enroll_user():
+    data = request.get_json()
+    user_id = data.get('user_id')
+    audio_urls = data.get('audio_urls', [])
+
+    if user_id and len(audio_urls) == 3:
+        try:
+            voice_id_system.enroll_user(user_id, audio_urls)
+            return jsonify({'status': 'success', 'message': 'User enrolled successfully'})
+        except Exception as e:
+            return jsonify({'status': 'error', 'message': str(e)})
+    else:
+        return jsonify({'status': 'error', 'message': 'Invalid request parameters'})
+
+@app.route('/authenticate', methods=['POST'])
+def authenticate_user():
+    data = request.get_json()
+    audio_url = data.get('audio_url')
+
+    if audio_url:
+        try:
+            user_id = voice_id_system.authenticate_user(audio_url)
+            if user_id:
+                return jsonify({'status': 'success', 'user_id': user_id, 'message': 'Authentication successful'})
+            else:
+                return jsonify({'status': 'error', 'message': 'Authentication failed'})
+        except Exception as e:
+            return jsonify({'status': 'error', 'message': str(e)})
+    else:
+        return jsonify({'status': 'error', 'message': 'Invalid request parameters'})
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0')
+    print("Flask server has started!")  # Add this line to print a message to the console when the server starts
---
+++ voice_identification.py
@@ -0,0 +1,80 @@
+import os
+import numpy as np
+import requests
+from io import BytesIO
+import librosa
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import accuracy_score
+import joblib
+
+class VoiceIdentification:
+    def __init__(self, model_file="voice_id_model.joblib"):
+        self.model_file = model_file
+        self.users_data = {}  # Dictionary to store user data
+        self.load_model()
+
+    def enroll_user(self, user_id, audio_files):
+        # Enroll a user by extracting features from their voice and storing the data
+        user_features = []
+
+        for audio_file in audio_files:
+
+            response = requests.get(audio_file)
+            if response.status_code == 200:
+                audio_bytes = BytesIO(response.content)
+                y, sr = librosa.load(audio_bytes)
+            else:
+                raise Exception(f"Failed to fetch audio from URL: {audio_file}")
+
+            # Extract features (Mel spectrogram)
+            mel_spectrogram = librosa.feature.melspectrogram(y=y, sr=sr)
+            features = np.mean(mel_spectrogram, axis=1)  # Use mean of each row as a feature
+
+            user_features.append(features)
+
+        # Store user data
+        self.users_data[user_id] = np.mean(user_features, axis=0)
+        self.save_model()
+
+    def authenticate_user(self, audio_file):
+        # Authenticate a user by comparing their voice features with enrolled users' data
+        # Load audio file
+        response = requests.get(audio_file)
+        if response.status_code == 200:
+            audio_bytes = BytesIO(response.content)
+            y, sr = librosa.load(audio_bytes)
+        else:
+            raise Exception(f"Failed to fetch audio from URL: {audio_file}")
+
+        # Extract features (Mel spectrogram)
+        mel_spectrogram = librosa.feature.melspectrogram(y=y, sr=sr)
+        features = np.mean(mel_spectrogram, axis=1)
+
+        # Compare with enrolled users' data
+        similarities = {}
+        for user_id, enrolled_features in self.users_data.items():
+            similarity = np.dot(features, enrolled_features) / (np.linalg.norm(features) * np.linalg.norm(enrolled_features))
+            similarities[user_id] = similarity
+
+        # Find the user with the highest similarity
+        predicted_user = max(similarities, key=similarities.get)
+        confidence = similarities[predicted_user]
+
+        # Set a threshold for confidence
+        threshold = 0.7
+        print(confidence)
+        if confidence > threshold:
+            return predicted_user
+        else:
+            return None
+
+    def save_model(self):
+        joblib.dump(self.users_data, self.model_file)
+
+    def load_model(self):
+        if os.path.exists(self.model_file):
+            self.users_data = joblib.load(self.model_file)
+
+# Example usage:
+
